e84695ca57a4d5104b7c0d6633c0207173fde482
Devanas17 <anasdeveloper17@gmail.com>
2023-06-07 22:25:22 +0530

    add factory test

--- contracts/compound/TestCompoundErc20.sol
+++ contracts/compound/TestCompoundErc20.sol
@@ -5,13 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ICompound.sol";
 
 contract TestCompoundErc20 {
+    IERC20 public immutable i_Ierc20;
+    CErc20 public immutable i_Cerc20;
 
-// IERC20 public immutable i_Ierc20;
-// CErc20 public immutable i_Cerc20;
-
-
-//     constructor(address _ierc20, address _cerc20) {
-//         i_Ierc20 = IERC20(_ierc20);
-//         i_Cerc20 = CErc20(_cerc20);
-    // }
+    constructor(address _ierc20, address _cerc20) {
+        i_Ierc20 = IERC20(_ierc20);
+        i_Cerc20 = CErc20(_cerc20);
+    }
 }
--- test/compound/TestCompoundErc20.test.js
+++ test/compound/TestCompoundErc20.test.js
@@ -1,4 +1,3 @@
-const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 describe("TestCompoundErc20", () => {
@@ -6,15 +5,15 @@ describe("TestCompoundErc20", () => {
     let user;
     let testCompoundErc20;
 
-    beforeEach(async () => {
-        [owner, user] = await ethers.getSigners();
+    // beforeEach(async () => {
+    //     [owner, user] = await ethers.getSigners();
 
-        const TestCompoundErc20 = await ethers.getContractFactory("TestCompoundErc20");
-        testCompoundErc20 = await Token.deploy(await owner.address, await user.address);
+    //     const TestCompoundErc20 = await ethers.getContractFactory("TestCompoundErc20");
+    //     testCompoundErc20 = await Token.deploy(await owner.address, await user.address);
 
-        await testCompoundErc20.deployed();
+    //     await testCompoundErc20.deployed();
 
-    });
+    // });
 
 
 })
