300b374bd09911aec1e0e757bca1bdb86140eb37
Devanas17 <anasdeveloper17@gmail.com>
2023-06-08 23:51:38 +0530

    add aave

---
+++ contracts/Aave/Supply&Borrow/MarketInteraction.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract MarketInteraction {
+    address public owner;
+
+    IPoolAddressesProvider public immutable i_addressProvider;
+    IPool public immutable POOL;
+    address private constant LINK_ADDRESS =
+        0x07C725d58437504CA5f814AE406e70E21C5e8e9e;
+    IERC20 private link;
+
+     constructor(address _addressProvider) {
+        i_addressProvider = IPoolAddressesProvider(_addressProvider);
+        POOL = IPool(i_addressProvider.getLendingPool());
+        owner = payable(msg.sender);
+        link = IERC20(LINK_ADDRESS);
+    }
+}
