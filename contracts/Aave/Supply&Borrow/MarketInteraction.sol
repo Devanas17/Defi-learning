653360ed4e530a9c728bccee0ff71556b3b65c74
Devanas17 <anasdeveloper17@gmail.com>
2023-06-28 23:09:55 +0530

    add Flashloan

--- contracts/Aave/Supply&Borrow/MarketInteraction.sol
+++ contracts/Aave/Supply&Borrow/MarketInteraction.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
@@ -9,14 +9,14 @@ contract MarketInteraction {
     address public owner;
 
     IPoolAddressesProvider public immutable i_addressProvider;
-    IPool public immutable POOL;
+    // IPool public immutable POOL;
     address private constant LINK_ADDRESS =
         0x07C725d58437504CA5f814AE406e70E21C5e8e9e;
     IERC20 private link;
 
      constructor(address _addressProvider) {
         i_addressProvider = IPoolAddressesProvider(_addressProvider);
-        POOL = IPool(i_addressProvider.getLendingPool());
+        // POOL = IPool(i_addressProvider.getLendingPool());
         owner = payable(msg.sender);
         link = IERC20(LINK_ADDRESS);
     }
